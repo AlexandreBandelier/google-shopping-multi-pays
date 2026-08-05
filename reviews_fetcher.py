08ce8e110ea2c930b07371df3ff23394eb1e4a5c
AlexandreBandelier <alexandre.bandelier@gmail.com>
2026-08-05 09:29:36 +0100

    Update reviews_fetcher.py

--- reviews_fetcher.py
+++ reviews_fetcher.py
@@ -1,11 +1,15 @@
 # reviews_fetcher.py
-import io
 import html
+import re
 import logging
+from datetime import datetime, timezone
 from woocommerce_fetcher import safe_api_get
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+# OPTIMISATION 1 : Pré-compilation de la Regex de nettoyage HTML hors de la boucle
+CLEAN_HTML_REGEX = re.compile(r'<[^>]+>')
 
 def fetch_all_product_reviews(wcapi):
     """
@@ -33,56 +37,92 @@ def fetch_all_product_reviews(wcapi):
     logger.info(f"Total avis récupérés : {len(all_reviews)}")
     return all_reviews
 
+def format_iso_timestamp(date_string):
+    """
+    Convertit la date WooCommerce au format ISO 8601 strict (ex: 2026-08-05T10:00:00Z).
+    """
+    if not date_string:
+        # OPTIMISATION 1 (suite) : Remplacement de datetime.utcnow() (déprécié)
+        return datetime.now(timezone.utc).strftime('%Y-%m-%dT%H:%M:%SZ')
+    try:
+        clean_date = date_string.replace('Z', '')
+        dt = datetime.fromisoformat(clean_date)
+        return dt.strftime('%Y-%m-%dT%H:%M:%SZ')
+    except Exception:
+        return datetime.now(timezone.utc).strftime('%Y-%m-%dT%H:%M:%SZ')
+
+def clean_cdata(text):
+    """
+    OPTIMISATION 2 : Échappe la séquence ']]>' pour éviter de casser la structure XML.
+    """
+    if not text:
+        return ""
+    return str(text).replace("]]>", "]]&gt;")
+
 def generate_reviews_xml(reviews, woo_url):
     """
-    Génère le flux XML au format officiel Google Product Reviews.
+    Génère le flux XML au format officiel Google Product Reviews (v2.3) optimisé.
     """
-    buffer = io.StringIO()
+    clean_woo_url = woo_url.rstrip('/')
     
-    buffer.write('<?xml version="1.0" encoding="UTF-8"?>\n')
-    buffer.write('<feed xmlns:vc="http://www.w3.org/2007/XMLSchema-versioning"\n')
-    buffer.write('      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"\n')
-    buffer.write('      xsi:noNamespaceSchemaLocation="http://www.google.com/shopping/reviews/reviews_schema.xsd">\n')
-    buffer.write('  <publisher>\n')
-    buffer.write('    <name>Votre Boutique</name>\n')
-    buffer.write(f'    <favicon>{woo_url}/favicon.ico</favicon>\n')
-    buffer.write('  </publisher>\n')
-    buffer.write('  <reviews>\n')
+    # En-tête XML
+    xml_parts = [
+        '<?xml version="1.0" encoding="UTF-8"?>\n',
+        '<feed xmlns:vc="http://www.w3.org/2007/XMLSchema-versioning"\n',
+        '      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"\n',
+        '      xsi:noNamespaceSchemaLocation="http://www.google.com/shopping/reviews/schema/product_reviews_schema.xsd">\n',
+        '  <version>2.3</version>\n',
+        '  <publisher>\n',
+        '    <name>Votre Boutique</name>\n',
+        f'    <favicon>{clean_woo_url}/favicon.ico</favicon>\n',
+        '  </publisher>\n',
+        '  <reviews>\n'
+    ]
     
     for rev in reviews:
         review_id = rev.get('id')
         product_id = rev.get('product_id')
         rating = rev.get('rating', 5)
-        reviewer_name = rev.get('reviewer', 'Anonyme')
-        date_created = rev.get('date_created', '').replace('T', ' ')
-        content = html.unescape(rev.get('review', ''))
+        reviewer_name = clean_cdata(rev.get('reviewer', 'Anonyme'))
         
-        # Nettoyage du HTML dans le commentaire
-        import re
-        content_clean = re.sub(r'<[^>]+>', ' ', content).strip()
+        timestamp = format_iso_timestamp(rev.get('date_created', ''))
+        
+        # Nettoyage HTML avec la Regex pré-compilée
+        content_raw = html.unescape(rev.get('review', ''))
+        content_clean = CLEAN_HTML_REGEX.sub(' ', content_raw).strip()
+        if not content_clean:
+            content_clean = "Avis client"
+        
+        content_clean = clean_cdata(content_clean)
 
-        buffer.write('    <review>\n')
-        buffer.write(f'      <review_id>{review_id}</review_id>\n')
-        buffer.write('      <reviewer>\n')
-        buffer.write(f'        <name><![CDATA[{reviewer_name}]]></name>\n')
-        buffer.write('      </reviewer>\n')
-        buffer.write(f'      <review_timestamp>{date_created}</review_timestamp>\n')
-        buffer.write('      <content><![CDATA[' + content_clean + ']]></content>\n')
-        buffer.write('      <ratings>\n')
-        buffer.write('        <overall min="1" max="5">' + str(rating) + '</overall>\n')
-        buffer.write('      </ratings>\n')
-        buffer.write('      <products>\n')
-        buffer.write('        <product>\n')
-        buffer.write('          <product_ids>\n')
-        buffer.write(f'            <g_id>{product_id}</g_id>\n')
-        buffer.write('          </product_ids>\n')
-        buffer.write('        </product>\n')
-        buffer.write('      </products>\n')
-        buffer.write('    </review>\n')
+        # Construction des URLs sécurisées
+        product_permalink = rev.get('product_permalink', clean_woo_url)
+        review_url = f"{product_permalink}#comment-{review_id}"
+
+        xml_parts.append('    <review>\n')
+        xml_parts.append(f'      <review_id>{review_id}</review_id>\n')
+        xml_parts.append('      <reviewer>\n')
+        xml_parts.append(f'        <name><![CDATA[{reviewer_name}]]></name>\n')
+        xml_parts.append('      </reviewer>\n')
+        xml_parts.append(f'      <review_timestamp>{timestamp}</review_timestamp>\n')
+        xml_parts.append(f'      <content><![CDATA[{content_clean}]]></content>\n')
+        xml_parts.append('      <review_urls>\n')
+        xml_parts.append(f'        <review_url type="singleton"><![CDATA[{review_url}]]></review_url>\n')
+        xml_parts.append('      </review_urls>\n')
+        xml_parts.append('      <ratings>\n')
+        xml_parts.append(f'        <overall min="1" max="5">{rating}</overall>\n')
+        xml_parts.append('      </ratings>\n')
+        xml_parts.append('      <products>\n')
+        xml_parts.append('        <product>\n')
+        xml_parts.append('          <product_ids>\n')
+        xml_parts.append(f'            <g_id>{product_id}</g_id>\n')
+        xml_parts.append('          </product_ids>\n')
+        xml_parts.append(f'          <product_url><![CDATA[{product_permalink}]]></product_url>\n')
+        xml_parts.append('        </product>\n')
+        xml_parts.append('      </products>\n')
+        xml_parts.append('    </review>\n')
         
-    buffer.write('  </reviews>\n')
-    buffer.write('</feed>')
+    xml_parts.append('  </reviews>\n')
+    xml_parts.append('</feed>')
     
-    xml_content = buffer.getvalue()
-    buffer.close()
-    return xml_content
+    return "".join(xml_parts)
